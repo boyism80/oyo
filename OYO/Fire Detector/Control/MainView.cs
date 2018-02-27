97015c3dd70c316f3a16a6bbd0250991a13f95d7
CSHYEON-PC\CSHYEON <boyism80@gmail.com>
2018-02-27 21:21:56 +0900

    메인뷰 최대화 배치 변경  - 해상도 고려 안함

--- OYO/Fire Detector/Control/MainView.cs
+++ OYO/Fire Detector/Control/MainView.cs
@@ -1,14 +1,21 @@
 ﻿using Fire_Detector.Control.SideTabView;
 using System;
 using System.Windows.Forms;
+using Fire_Detector.Source;
+using OpenCvSharp;
+using System.Drawing;
 
 namespace Fire_Detector.Control
 {
-    public partial class MainView : BaseTabView
+    public partial class MainView : BaseTabView, BunifuForm.MainForm.IStateChangedListener
     {
+        private int _basedHeight;
+
         public MainView()
         {
             InitializeComponent();
+
+            this._basedHeight = this.OYOPanel.Height;
         }
 
         private void bottomLabel_MouseEnter(object sender, EventArgs e)
@@ -77,6 +84,51 @@ namespace Fire_Detector.Control
 
             this.detectFireTabShow.Tag = this.Root.defaultView.sideExpandedBar.detectFireTab;
             this.detectFireLabel.Tag = this.detectFireTabShow;
+        }
+
+        public void OnStateChanged(bool connected)
+        {
+            
+        }
+
+        public void OnUpdated(UpdatedDataBuffer buffer, Mat updatedFrame, bool invalidated)
+        {
+            
+        }
+
+        public void OnSizeChanged(System.Drawing.Size size, bool isMaximize)
+        {
+            this.OYOPanel.Invoke(new MethodInvoker(delegate ()
+            {
+                this.OYOPanel.Height = (isMaximize ? (int)(this._basedHeight * 1.75f) : this._basedHeight);
+                this.OYOPanel.Padding = (isMaximize ? new Padding(80) : new Padding(40));
+            }));
+
+            this.bunifuCustomLabel1.Invoke(new MethodInvoker(delegate ()
+            {
+                this.bunifuCustomLabel1.Font = new Font(this.bunifuCustomLabel1.Font.FontFamily, isMaximize ? 26.0f : 18.0f, this.bunifuCustomLabel1.Font.Style);
+            }));
+
+            this.bunifuCustomLabel2.Invoke(new MethodInvoker(delegate ()
+            {
+                this.bunifuCustomLabel2.Font = new Font(this.bunifuCustomLabel2.Font.FontFamily, isMaximize ? 19.0f : 12.0f, this.bunifuCustomLabel2.Font.Style);
+                this.bunifuCustomLabel2.Padding = isMaximize ? new Padding(0, 6, 0, 6) : Padding.Empty;
+            }));
+
+            this.bunifuCustomLabel3.Invoke(new MethodInvoker(delegate ()
+            {
+                this.bunifuCustomLabel3.Font = new Font(this.bunifuCustomLabel3.Font.FontFamily, isMaximize ? 15.0f : 10.0f, this.bunifuCustomLabel3.Font.Style);
+            }));
+
+            this.bunifuCustomLabel4.Invoke(new MethodInvoker(delegate ()
+            {
+                this.bunifuCustomLabel4.Font = new Font(this.bunifuCustomLabel4.Font.FontFamily, isMaximize ? 15.0f : 10.0f, this.bunifuCustomLabel4.Font.Style);
+            }));
+
+            this.bunifuCustomLabel5.Invoke(new MethodInvoker(delegate ()
+            {
+                this.bunifuCustomLabel5.Font = new Font(this.bunifuCustomLabel5.Font.FontFamily, isMaximize ? 15.0f : 10.0f, this.bunifuCustomLabel5.Font.Style);
+            }));
         }
     }
 }
