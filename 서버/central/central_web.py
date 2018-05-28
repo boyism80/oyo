aa15b49647ee2a9fff081660af799875f362b43c
CSHYEON-PC\CSHYEON <boyism80@gmail.com>
2018-05-29 01:00:52 +0900

    중앙서버 업데이트

--- 서버/central/central_web.py
+++ 서버/central/central_web.py
@@ -12,91 +12,119 @@ conn    = None
 cursor  = None
 
 def generate_filename(directory, prefix, ext):
-	if not os.path.exists(directory):
-		os.mkdir(directory)
+    if not os.path.exists(directory):
+        os.mkdir(directory)
 
-	if not os.path.isdir(directory):
-		return None
+    if not os.path.isdir(directory):
+        return None
 
-	while True:
-		filename = '{}/{}_{}.{}'.format(directory, prefix, uuid.uuid4(), ext)
-		if not os.path.exists(filename):
-			return filename
+    while True:
+        filename = '{}/{}_{}.{}'.format(directory, prefix, uuid.uuid4(), ext)
+        if not os.path.exists(filename):
+            return filename
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/detection', methods=['POST'])
 def detection():
-	host = request.remote_addr
-	lat = int(request.form.get('lat'))
-	lon = int(request.form.get('lon'))
-	inf = request.files['inf']
-	vis = request.files['vis']
-	bnd = request.files['bnd']
-	inf_path = None
-	vis_path = None
-	bnd_path = None
+    host = request.remote_addr
+    lat = float(request.form.get('lat'))
+    lon = float(request.form.get('lon'))
+    tem = float(request.form.get('tem'))
+    inf = request.files['inf']
+    vis = request.files['vis']
+    thumb = request.files['thumb']
+    inf_path = None
+    vis_path = None
+    thumb_path = None
 
-	if inf:
-		content = inf.read()
-		inf_path = generate_filename('static/images', 'inf', 'jpg')
-		with open(inf_path, 'wb') as f:
-			f.write(content)
-		#inf.save(inf_path)
+    if inf:
+        content = inf.read()
+        inf_path = generate_filename('static/images', 'inf', 'jpg')
+        with open(inf_path, 'wb') as f:
+            f.write(content)
+        #inf.save(inf_path)
 
-	if vis:
-		content = vis.read()
-		vis_path = generate_filename('static/images', 'vis', 'jpg')
-		with open(vis_path, 'wb') as f:
-			f.write(content)
-		# vis.save(vis_path)
+    if vis:
+        content = vis.read()
+        vis_path = generate_filename('static/images', 'vis', 'jpg')
+        with open(vis_path, 'wb') as f:
+            f.write(content)
+        # vis.save(vis_path)
 
-	if bnd:
-		content = bnd.read()
-		bnd_path = generate_filename('static/images', 'bnd', 'jpg')
-		with open(bnd_path, 'wb') as f:
-			f.write(content)
-		# bnd.save(bnd_path)
+    if thumb:
+        content = thumb.read()
+        thumb_path = generate_filename('static/images', 'thumb', 'jpg')
+        with open(thumb_path, 'wb') as f:
+            f.write(content)
+        # bnd.save(bnd_path)
 
-	sql = "INSERT INTO detection (addr, lat, lon, infrared, visual, blended, date) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-	cursor.execute(sql, (str(host), lat, lon, inf_path, vis_path, bnd_path, time.strftime('%Y-%m-%d %H:%M:%S')))
-	conn.commit()
+    sql = "INSERT INTO detection (addr, lat, lon, temperature, thumb, infrared, visual, date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(sql, (str(host), lat, lon, tem, thumb_path, inf_path, vis_path, time.strftime('%Y-%m-%d %H:%M:%S')))
+    conn.commit()
 
-	return 'success'
+    return 'success'
 
-@app.route('/get', methods=['GET', 'POST'])
+@app.route('/gets', methods=['POST'])
+def gest():
+    offset = int(request.form.get('offset'))
+    count = int(request.form.get('count'))
+
+    sql = "SELECT id, lat, lon, temperature, thumb, date FROM detection WHERE deleted = 0 ORDER BY id DESC LIMIT %s, %s"
+    ret = {}
+    try:
+        ret['success'] = True
+        ret['data'] = []
+
+        count = cursor.execute(sql, (offset, count))
+        for element in cursor.fetchall():
+            item                = {}
+            id, lat, lon, tem, thumb, date = element
+        
+            item['id']          = id
+            item['temperature'] = float(tem)
+            item['position']    = {'lat': float(lat), 'lon': float(lon)}
+            item['date']        = str(date)
+            item['thumb']       = urllib.parse.urljoin(request.url_root, thumb)
+
+            ret['data'].append(item)
+    except Exception as e:
+        ret['success'] = False
+        ret['error'] = str(e)
+        if 'data' in ret:
+            del ret['data']
+
+    return json.dumps(ret)
+
+@app.route('/get', methods=['POST'])
 def get():
-	offset = int(request.form.get('offset'))
-	count = int(request.form.get('count'))
+    id = int(request.form.get('id'))
+    sql = "SELECT id, lat, lon, temperature, infrared, visual, date FROM detection WHERE id = %s AND deleted = 0 LIMIT 1"
+    
+    ret = {}
+    try:
+        ret['success'] = True
+        if cursor.execute(sql, (id,)) > 0:
+            id, lat, lon, tem, inf, vis, date = cursor.fetchone()
+            ret['data'] = {'id': id, 'position': {'lat': float(lat), 'lon': float(lon)}, 'tem': float(tem), 'inf': urllib.parse.urljoin(request.url_root, inf), 'vis': urllib.parse.urljoin(request.url_root, vis), 'date': str(date)}
+    except Exception as e:
+        ret['success'] = False
+        ret['error'] = str(e)
 
-	sql = "SELECT id, lat, lon, infrared, visual, blended, date FROM detection WHERE deleted = 0 ORDER BY id DESC LIMIT %s, %s"
-	count = cursor.execute(sql, (offset, count))
+    return json.dumps(ret)
 
-	ret = {'success': True, 'data': []}
-	for element in cursor.fetchall():
-		item                = {}
-		id, lat, lon, inf, vis, bnd, date = element
-		
-		item['id']          = id
-		item['position']    = {'lat': float(lat), 'lon': float(lon)}
-		item['date']        = str(date)
-		item['images']      = {'inf': urllib.parse.urljoin(request.url_root, inf), 'vis': urllib.parse.urljoin(request.url_root, vis), 'bnd': urllib.parse.urljoin(request.url_root, bnd)}
-
-		ret['data'].append(item)
-
-	return json.dumps(ret)
 
 
 if __name__ == '__main__':
-	global conn
-	global cursor
+    global conn
+    global cursor
 
-	conn = pymysql.connect(host='localhost', user='oyo', password='oyoteam', db='oyo', charset='utf8')
-	cursor = conn.cursor()
-	
-	app.run(host='0.0.0.0', port=8001)
+    conn = pymysql.connect(host='localhost', user='oyo', password='oyoteam', db='oyo', charset='utf8')
+    cursor = conn.cursor()
+    
+    app.run(host='0.0.0.0', port=8001)
 
-	cursor.close()
-	conn.close()
+    cursor.close()
+    conn.close()
